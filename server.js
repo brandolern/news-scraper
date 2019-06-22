86b8978cb0efd6af9dd73d32686baea48756dc21
brandolern <46857695+brandolern@users.noreply.github.com>
2019-06-22 09:47:21 -0600

    still fixing heroku

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.use(bodyParser.json());
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/3000";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-mongoose.connect("mongodb://localhost/scaper_db", {
-	useNewUrlParser: true
-});
+// mongoose.connect("mongodb://localhost/scaper_db", {
+// 	useNewUrlParser: true
+// });
 
 app.listen(PORT, function() {
 	console.log("App running on port " + PORT + "!");
