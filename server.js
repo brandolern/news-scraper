715b58598ddc71f163ef52237323ef0994ae4f2d
brandolern <46857695+brandolern@users.noreply.github.com>
2019-07-06 23:05:23 -0600

    deploying to heroku

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ app.set("view engine", "handlebars");
 require("./routes/router.js")(app);
 
 // app.use(express.urlencoded({ extended: true }));
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/3000";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/3000";
 
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-mongoose.connect("mongodb://localhost/scaper_db", {
-	useNewUrlParser: true
-});
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/scaper_db", {
+// 	useNewUrlParser: true
+// });
 
 app.listen(PORT, function() {
 	console.log("App running on port " + PORT + "!");
